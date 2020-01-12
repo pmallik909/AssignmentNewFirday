4c4f96d51ceec9be8ecfe10258deb821ba500f0d
Prabhudutta <prabhudutta@bluejeans.com>
2020-01-12 15:13:51 +0530

    Added ScreenShot and dependency

--- src/main/java/com/test/assignment/actions/UIActions.java
+++ src/main/java/com/test/assignment/actions/UIActions.java
@@ -2,14 +2,19 @@ package com.test.assignment.actions;
 
 import com.test.assignment.baseClass.BaseClass;
 import org.junit.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class UIActions{
+public class UIActions extends BaseClass{
 
     WebDriver driver;
     BaseClass baseClass = new BaseClass();
@@ -19,7 +24,7 @@ public class UIActions{
         baseClass.initializeTest();
     }
 
-    public void closeBrowser() throws Exception {
+    public void closeBrowser()  {
 
         baseClass.closeBrowser();
     }
@@ -43,6 +48,16 @@ public class UIActions{
 
         baseClass.navigatePage();
     }
+
+    public static void takeScreenShot() throws IOException {
+
+        File sourceFile = ((TakesScreenshot)BaseClass.driver).getScreenshotAs(OutputType.FILE);
+        String currentDirectory = System.getProperty("user.dir");
+
+        FileUtils.copyFile(sourceFile, new File(currentDirectory+ "/screenshots"+System.currentTimeMillis() + ".png"));
+
+    }
+
 
 
 }
